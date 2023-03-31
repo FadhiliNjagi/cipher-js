05c9e9298032e84a1f583c138d3ecc02cc92b1eb
Fadhili Njagi <fadhilinjagi@gmail.com>
2023-03-31 03:11:29 +0300

    Changed cipher encoding to base64. Added decodeKey method. Attempted to fix node < 6 compatibility. Updated workflows for thorough tests

---
+++ HISTORY.md
@@ -0,0 +1,17 @@
+2.0.0-dev
+===
+* Added `decodeKey(key, keyEncoding)`
+* Changed cipher encoding format from hex to base64, to create shorter strings
+* Attempt to fix compatibility with node < 6
+
+1.0.0
+===
+* Stable API
+* Cookie encryption and decryption
+* Security guidelines for encryption keys
+
+
+0.1.0 (deleted)
+===
+* Unstable API
+* README typos
--- test.ts
+++ test.ts
@@ -1,19 +1,37 @@
-import { encrypt, decrypt } from './index'
+import { encrypt, decrypt, decodeKey } from './index'
+
+describe('decodeKey = (key, keyEncoding)', function () {
+  it('should convert a string key to a buffer', function () {
+    const key = 'super secret key'
+    expect(decodeKey(key) instanceof Buffer).toBeTruthy()
+    const base64key = 'P6wVBCUaAnRlmBNG+1sNV9OY5N9KAyU6TH0ZJuQOmQc='
+    expect(decodeKey(base64key, 'base64') instanceof Buffer).toBeTruthy()
+    const hexKey = '3fac1504251a027465981346fb5b0d57d398e4df4a03253a4c7d1926e40e9907'
+    expect(decodeKey(hexKey, 'hex') instanceof Buffer).toBeTruthy()
+  })
+
+  it('should leave a buffer key intact', function () {
+    const key = Buffer.from('super secret key', 'utf8')
+    expect(decodeKey(key)).toBe(key)
+    const base64key = 'P6wVBCUaAnRlmBNG+1sNV9OY5N9KAyU6TH0ZJuQOmQc='
+    expect(decodeKey(base64key, 'base64') instanceof Buffer).toBeTruthy()
+  })
+})
 
 describe('decrypt(cipherText, key, keyEncoding)', function () {
   describe('when cipher format is invalid', function () {
     it('should throw an invalid format error', function () {
       expect(function () {
-        decrypt('foobar', '3fac1504251a027465981346fb5b0d57d398e4df4a03253a4c7d1926e40e9907')
-      }).toThrow(/Invalid cipher text format. Expected {32-character-iv}:{cipher-text}/)
+        decrypt('foobar', 'P6wVBCUaAnRlmBNG+1sNV9OY5N9KAyU6TH0ZJuQOmQc=')
+      }).toThrow(/Invalid cipher text format. Expected {24-character-iv}:{cipher-text}/)
     })
   })
 
   describe('when key is a string', function () {
     it('should decrypt cipher text', function () {
       const plainText = 'foobar'
-      const cipherText = 'a88b5ae04927b9aec7c274ef0848a9ad:2b776d451f03dc478f8a3bb70b41ca3e'
-      const secret = '3fac1504251a027465981346fb5b0d57d398e4df4a03253a4c7d1926e40e9907'
+      const cipherText = 'kI7KX7DpxGiQko4k2hPkaQ==:wN39aymkUhx8KVenmijULw=='
+      const secret = 'super secret key'
       expect(decrypt(cipherText, secret)).toBe(plainText)
     })
   })
@@ -21,8 +39,8 @@ describe('decrypt(cipherText, key, keyEncoding)', function () {
   describe('when key is a buffer', function () {
     it('should decrypt plain text', function () {
       const plainText = 'foobar'
-      const cipher = 'cf20922dd618009f4ee9172ac746701f:c7e37061e6486e3f574aae8f110e8831'
-      const secret = Buffer.from('3fac1504251a027465981346fb5b0d57d398e4df4a03253a4c7d1926e40e9907', 'hex')
+      const cipher = 'kI7KX7DpxGiQko4k2hPkaQ==:wN39aymkUhx8KVenmijULw=='
+      const secret = Buffer.from('P6wVBCUaAnRlmBNG+1sNV9OY5N9KAyU6TH0ZJuQOmQc=', 'base64')
       expect(decrypt(cipher, secret)).toBe(plainText)
     })
   })
@@ -32,10 +50,10 @@ describe('encrypt(plainText, key, keyEncoding)', function () {
   describe('when key is a string', function () {
     it('should encrypt plain text', function () {
       const plainText = 'foobar'
-      const secret = '3fac1504251a027465981346fb5b0d57d398e4df4a03253a4c7d1926e40e9907'
+      const secret = 'super secret key'
       const encrypted = encrypt(plainText, secret)
       expect(encrypted).not.toBe(plainText)
-      expect(encrypted).toMatch(/^[0-9a-f]{32}:[0-9a-f]+$/)
+      expect(encrypted).toMatch(/^[A-Za-z0-9+/=]{24}:[A-Za-z0-9+/=]+$/)
       expect(decrypt(encrypted, secret)).toBe(plainText)
     })
   })
@@ -43,12 +61,15 @@ describe('encrypt(plainText, key, keyEncoding)', function () {
   describe('when key is in different encoding', function () {
     it('should encrypt with the specified key encoding', function (done) {
       const plainText = 'foobar'
-      const hexSecret = '3fac1504251a027465981346fb5b0d57d398e4df4a03253a4c7d1926e40e9907'
-      const encryptedWithHex = encrypt(plainText, hexSecret, 'hex')
-      const encryptedWithUtf8 = encrypt(plainText, hexSecret)
-      expect(decrypt(encryptedWithHex, hexSecret, 'hex')).toBe(plainText)
+      const base64Secret = 'P6wVBCUaAnRlmBNG+1sNV9OY5N9KAyU6TH0ZJuQOmQc='
+      const encryptedWithBase64 = encrypt(plainText, base64Secret, 'base64')
+      const encryptedWithUtf8 = encrypt(plainText, base64Secret, 'utf8')
+      expect(decrypt(encryptedWithBase64, base64Secret, 'base64')).toBe(plainText)
       expect(function () {
-        decrypt(encryptedWithUtf8, hexSecret, 'hex')
+        decrypt(encryptedWithUtf8, base64Secret, 'base64')
+      }).toThrow(/bad decrypt/)
+      expect(function () {
+        decrypt(encryptedWithBase64, base64Secret, 'utf8')
       }).toThrow(/bad decrypt/)
 
       done()
@@ -58,10 +79,15 @@ describe('encrypt(plainText, key, keyEncoding)', function () {
   describe('when key is a buffer', function () {
     it('should encrypt plain text', function () {
       const plainText = 'foobar'
-      const secret = Buffer.from('3fac1504251a027465981346fb5b0d57d398e4df4a03253a4c7d1926e40e9907', 'hex')
-      const encrypted = encrypt(plainText, secret)
+      let secretBuffer = Buffer.from('3fac1504251a027465981346fb5b0d57d398e4df4a03253a4c7d1926e40e9907', 'hex')
+      let encrypted = encrypt(plainText, secretBuffer)
       expect(encrypted).not.toBe(plainText)
-      expect(decrypt(encrypted, secret)).toBe(plainText)
+      expect(decrypt(encrypted, secretBuffer)).toBe(plainText)
+
+      secretBuffer = Buffer.from('P6wVBCUaAnRlmBNG+1sNV9OY5N9KAyU6TH0ZJuQOmQc=', 'base64')
+      encrypted = encrypt(plainText, secretBuffer)
+      expect(encrypted).not.toBe(plainText)
+      expect(decrypt(encrypted, secretBuffer)).toBe(plainText)
     })
   })
 
@@ -69,7 +95,7 @@ describe('encrypt(plainText, key, keyEncoding)', function () {
     it('should encrypt plain text with sha256 of key', function () {
       const plainText = 'foobar'
       const shortSecret = 'super secret key'
-      const sha256Secret = Buffer.from('3fac1504251a027465981346fb5b0d57d398e4df4a03253a4c7d1926e40e9907', 'hex')
+      const sha256Secret = Buffer.from('P6wVBCUaAnRlmBNG+1sNV9OY5N9KAyU6TH0ZJuQOmQc=', 'base64')
       const encryptedWithShortSecret = encrypt(plainText, shortSecret)
       const encryptedWithSha256Secret = encrypt(plainText, sha256Secret)
       // Assert that the keys are interchangeable
